814924a2a88766c18f52dbcd10c0d1392e973464
Vinícius <vini8cs@gmail.com>
2024-04-07 21:50:07 -0300

    new commnets

--- workflow/scripts/github.py
+++ workflow/scripts/github.py
@@ -3,4 +3,4 @@
 #git push  origin main
 #git log --oneline #checar todos os commits e códigos
 #git rebase -i HEAD~3 #juntar os 3 últimso commits. também pode usar o código do quarto, neste caso.
-#você vai deixar um "pick" e mudar os outros que vai querer juntar para "s" e depois criar um commit novo para os três meslcados.
+#você vai deixar um "pick" e mudar os outros que vai querer juntar para "s" e depois apagar os commits antigos e criar um commit novo para os três meslcados.
